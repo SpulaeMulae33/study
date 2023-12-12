c341aeb99a699fd854935d5deeebf85f6079e733
SpulaeMulae33 <dolgov.mase777@gmail.com>
2023-12-12 02:28:26 -0500

    new

--- pr7.py
+++ pr7.py
@@ -1,11 +1,16 @@
-import random 
-print("Enter list size: ")
-s = int(input())
-a = []
-while len(a) < s:
-    a.append(random.randint(1,30))
-M = max(a)
-m = min(a)
-b = list(map(lambda x: x if x > M or x < m else 0, a))
+import random
 
-print("U'r list: \n", a, b)
+n = int(input('Input N: '))
+
+rand_list = [random.randint(1,30) for i in range (n)]
+print(f"You'r list: {rand_list}")
+
+min_val = min(rand_list)
+max_val = max(rand_list)
+min_index = rand_list.index(max_val)
+max_index = rand_list.index(min_val)
+start_index = min(min_index, max_index) + 1
+end_index = max(min_index, max_index)
+
+rand_list[start_index:end_index] = [0] * (end_index - start_index)
+print(f"Modified list: {rand_list}")
