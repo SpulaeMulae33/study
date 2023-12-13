bfc2f0eb54b4474bae754f55dcdf1f7f30ef2e9f
SpulaeMulae33 <dolgov.mase777@gmail.com>
2023-12-13 07:42:51 -0500

    pr10 in progress

--- pr10.py
+++ pr10.py
@@ -6,16 +6,18 @@ A = [ [0]* M for i in range(M)]
 
 for i in range(N):
     for j in range(M):
-        A[i][j] = random.randint(1,100)
+        A[i][j] = random.randint(10,99)
 
 for i in range(len(A)):
     for j in range(len(A[i])):
-        print(A[i][j], end = ' ')
+        if len(A[i]) == 2:
+            print(A[i][j], end = ' ')
+        else:
+            print(A[i][j], end = '   ')
     print()
 
 string = len(A)
 row = len(A[i])
 
-for i in range(len(A)):
-    for j in range(len(A[i])):
-        
+#for i in range(len(A)):
+#    for j in range(len(A[i])):
